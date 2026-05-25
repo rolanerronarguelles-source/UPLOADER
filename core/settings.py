9da99d2f132060a2183c2fa1d7c39c94124aa577
rolanerronarguelles-source <rolanerron.arguelles@evsu.edu.ph>
2026-05-25 21:02:48 +0800

    fix static files collection

--- core/settings.py
+++ core/settings.py
@@ -33,8 +33,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage', 
     'django.contrib.staticfiles', # staticfiles stays here
+    'cloudinary_storage', 
     'cloudinary',
     'albums',
 ]
